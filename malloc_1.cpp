20a6984356b03a98a940b6d9a5af6fb825fc7abc
taligrayzel <tali.grayzel@campus.technion.ac.il>
2023-06-28 20:04:37 +0300

    first part

---
+++ malloc_1.cpp
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+#define MAX_ALLOCATE 100000000
+
+void* smalloc (size_t size)
+{
+    if(size==0 || size>MAX_ALLOCATE)
+    {
+        return NULL;
+    }
+    void* beginBlock = sbrk(size);
+    if (beginBlock == (void*)-1)
+    {
+        return NULL;
+    }
+    return beginBlock;
+}
