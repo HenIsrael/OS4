3c03f501789b0920b3f72ec3de15f431331e5e06
HenIsrael <hen.israel@campus.technion.ac.il>
2023-06-29 14:27:09 +0300

    implement new fun

--- malloc_2.cpp
+++ malloc_2.cpp
@@ -19,12 +19,14 @@ struct List {
 
     List():head(nullptr),tail(nullptr){}
     void* insert_block(size_t size);
+    void free_space(void* address);
     size_t get_num_free_blocks();
     size_t get_num_free_bytes();
     size_t get_num_allocated_blocks();
     size_t get_num_allocated_bytes();
     size_t get_num_metadata_bytes();
     size_t get_size_metadata();
+    size_t get_block_size(void* address);
 
 };
 
@@ -110,6 +112,25 @@ void* List::insert_block(size_t size)
     }
 }
 
+void List::free_space(void* address){
+
+    if(!head){
+        return;
+    }
+
+    MallocMetadata *runner = head;
+    while(runner){
+        if(runner->start_address==address && runner->is_free==false){
+            runner->is_free = true;
+            return;
+        }
+
+        runner = runner->next;
+    }
+
+    return;
+}
+
 size_t List::get_num_free_blocks(){
 
 }
@@ -129,11 +150,28 @@ size_t List::get_size_metadata(){
 
 }
 
+size_t List::get_block_size(void* address){
+
+    if (!head){
+        return 0;
+    }
+
+    MallocMetadata *runner = head;
+    while(runner){
+        if(runner->start_address=address){
+            return runner->size;
+        }
+        runner = runner->next;
+    }
+
+    return 0;
+}
+
 // -------------------- End Of Blocks List functions Implementation --------------------  //
 
 
 // global pointer to the blocks list
-List* List_pointer = (List *) sbrk(sizeof(List));;
+List* List_pointer = (List *) sbrk(sizeof(List));
 
 // -------------------- Basic Malloc functions --------------------  //
 
@@ -148,19 +186,57 @@ void* smalloc(size_t size)
     return List_pointer->insert_block(size);
 }
 
-
 void *scalloc(size_t num, size_t size) {
-    // TODO : add mplementation 
+
+    size_t bytes = num * size;
+    void *return_addr = smalloc(bytes);
+    if(!return_addr){
+        return nullptr;
+    }
+
+    memset(return_addr,0,bytes);
+    return return_addr;
 
 }
 
 void sfree(void *p) {
-    // TODO : add mplementation
+    
+    if (!p){
+        return;
+    }
+
+    List_pointer->free_space(p);
 
 }
 
+
 void *srealloc(void *oldp, size_t size) {
-    // TODO : add mplementation
+
+    if(size==0 || size>MAX_ALLOCATE)
+    {
+        return nullptr;
+    }
+    if(!oldp){
+        return smalloc(size);
+    }
+
+    size_t block_size_in_addr = List_pointer->get_block_size(oldp);
+
+    if(size <= block_size_in_addr){
+        return oldp;
+    }
+
+    //need to allocate new block and copy the old metadata to the new
+
+    void* new_block_addr = smalloc(size);
+    if(!new_block_addr){
+        return nullptr;
+    }
+
+    memmove(new_block_addr, oldp, block_size_in_addr);
+    sfree(oldp);
+    return new_block_addr;
+    
 }
 
 size_t _num_free_blocks() {
